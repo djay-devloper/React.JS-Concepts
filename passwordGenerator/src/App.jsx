3673aacb497a8bd55443fcbdc6ff5243c8f9038c
djay-devloper <djay.developer@gmail.com>
2026-03-31 10:37:31 +0530

    ...

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useState, useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
@@ -8,11 +8,17 @@ function App() {
   const[symbolAllowed, setSymbolAllowed]= useState(false)
   const[password, setPassword]= useState("")
 
+
+  //Using useRef hook to target the password and copying it on clipboard.
+  //useRef is a React hook that lets you store a value that stays the same between renders like HTML elements.
+  let passwordRef= useRef(null) 
+
+
   // It is a react hook which prevents from unneccessary re-rendering
   // useCallback will return a memoized version of the callback that only changes if one of the dependecy has changed.
   const passwordGenerator= useCallback(()=>{
-    const pass= ""
-    const str= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    let pass= ""
+    let str= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
     if(numberAllowed) str+= "0123456789"
 
@@ -29,19 +35,42 @@ function App() {
 
   }, [length, numberAllowed, symbolAllowed, setPassword])
 
+
+  // Think of it like- “After React shows the UI, now do this extra work.”
+  // Used for API calls and timers.
+  useEffect(()=>{
+    passwordGenerator()
+  }, [length, numberAllowed, symbolAllowed, passwordGenerator])
+
+  const copyPasswordToClipboard= useCallback(()=>{
+    passwordRef.current?.select()
+    passwordRef.current?.setSelectionRange(0, 999)
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+  
+
   return (
     <>
 
-      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700 text-center'>
-        <h2 className='text-center text-white'>Password Generator</h2>
+      <div className="w-full max-w-md h-[130px] mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700 text-center">
+
+        <h1 className='text-center text-white my-5'>Password Generator</h1>
 
         {/* Placeholder and copy button. */}
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
-          <input type="text" value={password} className='outline-none w-full py-1 px-3' placeholder='Password' readOnly/>
-          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
+          <input type="text"
+          value={password}
+          className='outline-none w-full py-1 px-3 bg-white'
+          placeholder='Password'
+          readOnly
+          //useRef will use it as reference 
+          ref= {passwordRef}/>
+
+          <button onClick={copyPasswordToClipboard}
+          className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 cursor-pointer'>Copy</button>
         </div>
 
-        {/* div of length number and symbol */}
+        {/* div of length numbers and symbols */}
         <div className='flex text-sm gap-x-2'>
 
           <div className='flex items-center gap-x-1'>
@@ -58,6 +87,7 @@ function App() {
             type="checkbox"
             defaultChecked= {numberAllowed}
             id='numberInput'
+            className='cursor-pointer'
             onChange={()=>{
               setNumberAllowed((prev)=> !prev);
             }}/>
@@ -68,6 +98,7 @@ function App() {
             <input type="checkbox"
             defaultChecked= {symbolAllowed}
             id='symbolInput'
+            className='cursor-pointer'
             onChange={()=>{
               setSymbolAllowed((prev)=> !prev);
             }}/>
