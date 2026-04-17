8f51b3815790f236a0b2b3c10c728c8e887fc709
Dhananjay Yadav <djyadav639@djays-MacBook.local>
2026-04-17 20:24:01 +0530

    tailwind and react-router-dom configured

---
+++ 07reactrouter/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-white bg-blue-600 p-6 rounded-lg shadow-lg">
+        tailwind checking
+      </h1>
+    </>
+  )
+}
+
+export default App
