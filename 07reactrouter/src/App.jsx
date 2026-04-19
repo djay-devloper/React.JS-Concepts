8e842c05b0a5a85c0f164bf7a2f8535c088d33be
Dhananjay Yadav <djyadav639@djays-MacBook.local>
2026-04-19 19:44:51 +0530

    ..

--- 07reactrouter/src/App.jsx
+++ 07reactrouter/src/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from 'react'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 
-// When you make nested routes in React Router, the parent component needs some place where the matched child 
+// When you make nested routes in React Router, the parent component needs some place where the child 
 // component should appear. That place is <Outlet />
 import { Outlet } from 'react-router-dom'
 
@@ -22,5 +21,6 @@ export default App
 // We are not gonna render App.jsx in main.jsx directly since we have congigured router-dom and we render the router. 
 // Because now the router controls the UI. It decides which route/component to show.
 
-// Here App.jsx is used as a layout componenet, like for every page home, about, contact us the header and footer part
-// is same. outlet is changing according to route.
+// Here App.jsx is used as a layout/root componenet, like for every page home, about, contact the header and footer 
+// part is same. 
+// outlet is like a placeholder where child routes are rendered.
