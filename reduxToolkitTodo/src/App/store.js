0cc0389fe4632b027b153e6843a928b124026f5b
Dhananjay Yadav <djyadav639@Mac.lan>
2026-06-15 16:30:53 +0530

    store and slice created

--- reduxToolkitTodo/src/App/store.js
+++ reduxToolkitTodo/src/App/store.js
@@ -1,3 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
+import { todoSlice } from '../features/todo/todoSlice';
 
-export const store= configureStore({})
+export const store= configureStore({
+    reducer: todoSlice.reducer
+})
+
+
+
+// 4 things to learn: store, reducers, useselector, usedispatch.
---
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -0,0 +1,29 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+// how store look like initialyy.
+const initialState= {
+    todos: [{id: 1, msg: "Hello World"}]
+}
+
+// creating slice which is holding reducers and other tools.
+export const todoSlice= createSlice({
+    name: "todo",
+    initialState,
+
+    reducers: {
+        addTodo: (state, action) => {
+            const todo= {
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+            state.todos= state.todos.filter((todo)=> todo.id!== action.payload)
+        }
+    }
+})
+
+// exporting reducers explicitaly.
+export const {addTodo, removeTodo} = todoSlice.actions
+export default todoSlice.reducer
