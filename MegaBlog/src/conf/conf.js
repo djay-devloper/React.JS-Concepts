b5a20e13c78350681116709b41b11862d0781fd4
Dhananjay Yadav <djyadav639@djays-MacBook.local>
2026-06-20 19:55:04 +0530

    updated

--- MegaBlog/src/conf/conf.js
+++ MegaBlog/src/conf/conf.js
@@ -1,7 +1,7 @@
 const conf= {
     appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appwriteDatabaseid: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteTableId: String(import.meta.env.VITE_APPWRITE_TABLE_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 
