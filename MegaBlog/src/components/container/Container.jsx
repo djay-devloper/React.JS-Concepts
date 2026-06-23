7638c97d4df19241ea63240c6e52c5e451c175dc
Dhananjay Yadav <djyadav639@djays-MacBook.local>
2026-06-23 15:13:25 +0530

    footer and container added

---
+++ MegaBlog/src/components/container/Container.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// container will contain some component as it.
+function Container(){
+    return (
+        <div className='w-full max-w-7xl mx-auto px-4'>{children}</div>
+    )
+}
+
+export default Container
