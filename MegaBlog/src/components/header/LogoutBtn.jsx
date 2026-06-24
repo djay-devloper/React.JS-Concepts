42c634c41e1bc59a4fae890d1472ddf0654deb36
Dhananjay Yadav <djyadav639@djays-MacBook.local>
2026-06-24 10:58:13 +0530

    logout button for everywhere

---
+++ MegaBlog/src/components/header/LogoutBtn.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {useDispatch} from 'react-redux'
+import authService from '../../appwrite/auth'
+import {logout} from '../../store/authSlice'
+
+function LogoutBtn(){
+    const dispatch= useDispatch()
+    const logoutHandler = () => {
+        authService.logout().then(() => {
+            dispatch(logout())
+        })
+    }
+    return(
+        <button
+        className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+        onClick={logoutHandler}
+        >Logout</button>
+    )
+}
+
+export default LogoutBtn
+
+// You will likely need a logout button in more than one place in your application. It might live in a top navigation bar, a mobile slide-out menu, and a "User Settings" page.
