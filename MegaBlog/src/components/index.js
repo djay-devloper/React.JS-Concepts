efaa3c789ddff3b12ae500bd70234895fb47eadf
Dhananjay Yadav <djyadav639@Mac.lan>
2026-07-01 11:11:12 +0530

    ..

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -3,10 +3,13 @@ import Footer from "./footer/Footer"
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./header/LogoutBtn";
+import RTE from "./RTE";
 
 export{
     Header,
     Footer,
     Container,
-    Logo
+    Logo,
+    LogoutBtn,
+    RTE
 }
